ff55de568393889d4227791ffc690bbb5c75fda2
u-master <yurgan@gmail.com>
2020-10-30 15:45:12 +0600

    make hometask-2

--- src/example.ts
+++ src/example.ts
@@ -1,1 +1,28 @@
 // Example Typescript
+
+// Работа с простыми типами
+type TConcatFunc = (str1: string, str2: string) => string ;
+const concat: TConcatFunc = (str1, str2) => `${str1}${str2}`;
+
+const message = concat('Hello, ', 'Zar!');
+
+// Работа с интерфейсами
+interface IHometask {
+    howIDoIt: string;
+    simeArray: (string | number)[];
+    withData?: IHometask[];
+};
+
+const MyHometask: IHometask = {
+	howIDoIt: "I Do It Wel",
+	simeArray: ["string one", "string two", 42],
+	withData: [{ howIDoIt: "I Do It Wel", simeArray: ["string one", 23] }],
+}
+
+// Типизация функций, используя Generic
+interface MyArray<T> {
+	[N: number]: T;
+
+	reduce<AccT>(fn: (acc: AccT, elem: T, index: number) => AccT, initial: AccT): AccT;
+}
+
